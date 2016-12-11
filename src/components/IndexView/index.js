d773bdc247454a2983d03b6b3062d4bde9fe9eb4
hydracoder76 <jwcrazy90@gmail.com>
2016-12-11 17:09:50 +0530

    add first time view to authorized view

--- src/components/IndexView/index.js
+++ src/components/IndexView/index.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { observer, inject } from 'mobx-react';
 import UnauthorizedView from './unauthorizedView';
 import AuthorizedView from './authorizedView';
+import FirstTimeView from './firstTimeView';
 import { signIn, signUp } from '../../services/api';
 
-const IndexViewContainer = inject('authStore')(observer(( { authStore }) => {
+const IndexViewContainer = inject('authStore', 'accountStore')(observer(( { authStore, accountStore }) => {
   // TODO: based on jwt token render the appropriate views.
 
   return(
     authStore.jwt?
-    <AuthorizedView
-      jwt={authStore.jwt}
-    />
+      accountStore.accounts.length === 0 ?
+      <FirstTimeView { ...accountStore }
+      /> :
+      <AuthorizedView
+        jwt={authStore.jwt}
+        accounts={accountStore.accounts}
+      />
     :
     <UnauthorizedView
       jwt={authStore.jwt}
