2273621a58e7d94fee5620f22e3378a6254ef363
hydracoder76 <jwcrazy90@gmail.com>
2016-12-17 00:36:56 +0530

    enable search in the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,36 +1,45 @@
 import React from 'react'
-import { Checkbox, Menu,  Input, List, Image} from 'semantic-ui-react';
+import { Menu,  Input, List} from 'semantic-ui-react';
+import SidebarItem from '../SidebarItem';
 
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchInput: ""};
+  }
+
+  getFilteredAccounts = (accounts, searchInput="") => {
+    let regex = new RegExp(`[A-Za-z0-9]?${searchInput}[A-Za-z0-9]?`)
+    return this.props.accounts.filter((account) => {
+      return (
+        regex.test(account.name.toLowerCase()) ||
+        regex.test(account.remote_source.toLowerCase()) ||
+        regex.test(account.account_type.toLowerCase())
+      );
+    });
+  }
+
+  renderSidebarItems = () => {
+    let accounts = this.getFilteredAccounts(this.props.accounts, this.state.searchInput);
+    return accounts.map((account) =>{
+      return <SidebarItem key={account.id} {...this.props} account={account}/>
+    })
+  }
+
   render () {
     // const activeItem = "home";
+
     return(
       <Menu vertical fixed="left" style={{padding: 0, width: 300}}>
         <Menu.Item header as='h1'>Shareito</Menu.Item>
         <Menu.Item>
-          <Input icon='search' placeholder='Search Account..' />
+          <Input
+            icon='search'
+            placeholder='Search Account..'
+            onChange={(e, {value}) => {this.setState({searchInput: value})}}
+          />
           <List relaxed divided size='huge'>
-            <List.Item>
-              <Image avatar src='http://semantic-ui.com/images/avatar/small/helen.jpg' />
-              <List.Content>
-                <List.Header>Snickerdoodle </List.Header>
-                <div className="sidebar-subtext">Twitter</div>
-              </List.Content>
-              <List.Content floated='right'>
-                <Checkbox toggle className=""></Checkbox>
-              </List.Content>
-            </List.Item>
-            <List.Item>
-              <Image avatar src='http://semantic-ui.com/images/avatar/small/daniel.jpg' />
-              <List.Content>
-                <List.Header>Poodle</List.Header>
-                <div className="sidebar-subtext">Facebook</div>
-              </List.Content>
-              <List.Content floated='right'>
-                <Checkbox toggle className=""></Checkbox>
-              </List.Content>
-
-            </List.Item>
+          {this.renderSidebarItems()}
           </List>
         </Menu.Item>
       </Menu>
